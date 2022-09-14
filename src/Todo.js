23e781fd77cd76d1e4c7233e47db332d57b5359e
AbhidattaShetty <108416589+AbhidattaShetty@users.noreply.github.com>
2022-09-14 15:56:31 +0530

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -3,75 +3,73 @@ import { useState } from "react";
 const Todo = () => {
 
     const [inputdata, setInputData] = useState("");
-    const [items, setItems]= useState([]);
+    const [items, setItems] = useState([]);
 
-    {/*add the items function */}
-    const addItem = ()=>{
-        if(!inputdata){
-            alert("Please fill the data");
-        } else {
-            const myNewInputData = {
-                id : new Date().getTime().toString(),
-                name: inputdata,
-            }
-            setItems([...items, myNewInputData])
-            setInputData("");
-        }
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputData(newValue);
     }
 
-    {/*how to delete item section */}
+    {/*how to Add item section */ }
+    function addItem() {
+        setItems((prevItems) => {
+            return [...prevItems, inputdata];
+        });
+        setInputData("");
+    }
 
-    const deleteItem = (index) => {
-        const updatedItems = items.filter((curElem) => {
-          return curElem.id !== index;
+    {/*how to delete item section */ }
+
+    const deleteItem = (id) => {
+        const updatedItems = items.filter((curElem, index) => {
+            return index !== id;
         });
         setItems(updatedItems);
-      };
+    };
 
-  {/*remove all the elements */}
-      
-   const removeAll = () => {
-     setItems([]);
-   };
+    {/*remove all the elements */ }
 
+    const removeAll = () => {
+        setItems([])
+    }
     return (
         <>
             <div className="main-div">
                 <div className="child-div">
                     <figure>
-                        <img src="./images/todo.png" alt="todologo"/>
+                        <img src="./images/todo.png" alt="todologo" />
                         <figcaption>Add Your List Here</figcaption>
                     </figure>
                     <div className="addItems">
-                    <input type="text" placeholder="✍ Add Item" className="form-control"
-                      value={inputdata}
-                      onChange={(event) => setInputData(event.target.value)}
-                    />
+                        <input type="text" placeholder="✍ Add Item" className="form-control"
+                            value={inputdata}
+                            onChange={handleChange}
+                        />
                         <i className="fa fa-plus add-btn" onClick={addItem}></i>
                     </div>
 
                     {/* Show our items */}
                     <div className="showItems">
-                    {items.map((curElem)=>{
-                        return(
-                            <div className="eachItem" key={curElem.id}>
-                            <h3>{curElem.name}</h3>
-                            <div className="todo-btn">
-                            {/*<i className="fa fa-edit add-btn"></i>*/}
-                            <i className="fa fa-trash-alt add-btn" onClick={() => deleteItem(curElem.id)}></i>
-                            </div>
-                        </div>
-                        )
-                    })}
-                       
+                        {items.map((curElem, index) => {
+                            return (
+                                <div className="eachItem" key={index}>
+                                    <h3>{curElem}</h3>
+                                    <div className="todo-btn">
+                                        {/*<i className="fa fa-edit add-btn"></i>*/}
+                                        <i className="fa fa-trash-alt add-btn" onClick={() => deleteItem(index)}></i>
+                                    </div>
+                                </div>
+                            )
+                        })}
+
                     </div>
 
 
                     {/* Remove All button */}
                     <div className="showItems">
-                    <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}>
-                    <span>CHECK LIST</span>
-                    </button>
+                        <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}>
+                            <span>CHECK LIST</span>
+                        </button>
                     </div>
                 </div>
             </div>
